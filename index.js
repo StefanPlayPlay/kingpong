08c8eda855c40f3d5509d951b65d5e167cd70571
Stefan Lentner <stefan@playplay.com>
2022-12-06 11:29:41 +0100

    feat: add body parser

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 let express = require('express')
 let http = require('http')
 let path = require('path')
+const bodyParser = require('body-parser');
+
 
 let app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res, next) {
-  let  data = {
-    content: 'Hello world!',
-    title: 'Bootstrap example'
-  }
-  return res.json({"body":"coucou"});
-})
+app.post('/', (req, res) => {
+    console.log('Got body:', req.body);
+    res.sendStatus(200);
+});
 
 let server = http.createServer(app)
 
