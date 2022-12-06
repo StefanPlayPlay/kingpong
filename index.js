fbb9e548c29ba982df812cde94be97aa9ff3f866
Thomas Calvet <thomas.calvet@playplay.com>
2022-12-06 14:23:22 +0100

    basic modal ex

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ let dotenv =  require('dotenv');
 
 dotenv.config();
 
+const { WebClient } = require('@slack/web-api');
+
+// Read a token from the environment variables
+const token = process.env.SLACK_TOKEN;
+
+// Initialize
+const slack = new WebClient(token);
+
 let app = express()
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     console.log('Got body:', req.body);
     let userA = req.body.user_id;
 
@@ -19,6 +27,18 @@ app.post('/', (req, res) => {
     let userB = match[1];
     let scoreA = match[2];
     let scoreB = match[3];
+
+    const conversation = await slack.conversations.open({
+        users: userB,
+    });
+
+    await slack.chat.postMessage({
+        channel: conversation.channel.id,
+        text: 'Hello, this is a private message!',
+        attachments: [
+
+        ],
+    });
 
     return res.json({
         "channel": "C04DX31AXD0",
@@ -41,6 +61,74 @@ app.post('/', (req, res) => {
             }
         ]
     });
+});
+
+app.post('/interact', async (req, res) => {
+    console.log('Got body:', req.body);
+    const data = JSON.parse(req.body.payload);
+
+    if ('report_result' === data['callback_id']) {
+        // Define the modal configuration
+        const modal = {
+            type: 'modal',
+            callback_id: "my_custm_cb_id",
+            title: {
+                type: 'plain_text',
+                text: 'Report your result'
+            },
+            blocks: [
+                {
+                    "type": "input",
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Opponent"
+                    },
+                    "element": {
+                        "action_id": "text1234",
+                        "type": "users_select"
+                    }
+                },
+                {
+                    "type": "input",
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Your score"
+                    },
+                    "element": {
+                        "action_id": "text1234",
+                        "type": "number_input",
+                        is_decimal_allowed: false,
+                        "min_value": "0"
+                    }
+                },
+                {
+                    "type": "input",
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Opponent score"
+                    },
+                    "element": {
+                        "action_id": "text1234",
+                        "type": "number_input",
+                        is_decimal_allowed: false,
+                        "min_value": "0"
+                    }
+                },
+            ],
+            submit: {
+                type: 'plain_text',
+                text: 'Submit'
+            }
+        };
+
+        // Open the modal
+        await slack.views.open({
+            trigger_id: data.trigger_id,
+            view: modal
+        });
+    }
+
+    return res.json({});
 });
 
 let server = http.createServer(app)
