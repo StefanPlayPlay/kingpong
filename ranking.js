306a2710fd4e1145864db45e16ba0bbc18a60515
Stefan Lentner <stefan@playplay.com>
2022-12-07 09:42:45 +0100

    feat: add ranking update function

--- ranking.js
+++ ranking.js
@@ -1,5 +1,14 @@
+const Arpad = require('arpad');
+
+const elo = new Arpad();
+
 const updateRanking = (winner, looser)=>{
-    console.log('updated ranking');
+    const winnerCurrentRating = 1200;
+    const looserCurrentRating = 1250;
+    const updatedWinnerRating = elo.newRatingIfWon(winnerCurrentRating,looserCurrentRating);
+    const updatedLooserRating = elo.newRatingIfLost(looserCurrentRating, winnerCurrentRating);
+    console.log(`winner went from ${winnerCurrentRating} to ${updatedWinnerRating}`),
+    console.log(`winner went from ${looserCurrentRating} to ${updatedLooserRating}`);
 }
 
 modules.exports = {
