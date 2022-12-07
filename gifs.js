5b1ca6aeb1d12a7ed6196a9a0abacc842dd1ef01
Vivien <vivien.blum@free.fr>
2022-12-07 13:50:17 +0100

    Create gifs.js

---
+++ gifs.js
@@ -0,0 +1,39 @@
+const gifs = {
+    1: [
+        "https://media.giphy.com/media/2vs70gBAfQXvOOYsBI/giphy.gif",
+    ],
+    2: [
+        "https://i.imgur.com/Od9OtO0.gif",
+    ],
+    3: [
+        "https://i.imgur.com/E1T82Vq.gif",
+    ],
+    4: [
+        "https://i.imgur.com/LeZX32R.gif",
+    ],
+    5: [
+        "https://i.giphy.com/media/hQSkQm41brwdYDIGqr/giphy.webp",
+    ],
+    6: [
+        "https://i.imgur.com/z0mssC3.gif",
+    ],
+    7: [
+        "https://media.giphy.com/media/sxCKezAUq8yn6/giphy.gif",
+    ],
+    8: [
+        "https://media.giphy.com/media/Oc4pSGTekXrtC/giphy.gif",
+    ],
+    9: [
+        "https://i.imgur.com/nEHwwmz.gif",
+    ],
+    10: [
+        "https://media.giphy.com/media/l2JJod66TKus2t3QA/giphy.gif",
+    ],
+    11: [
+        "https://media.giphy.com/media/9QmldqOBxeBZC/giphy.gif",
+    ],
+};
+
+module.exports = {
+    gifs
+}
